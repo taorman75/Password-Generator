425ca70d4333ff562cd15a29341c06043c4dc04a
Queen of Corgis <tana.owens@gmail.com>
2020-06-25 19:02:54 -0600

    added js file edits, including setting variables

--- script.js
+++ script.js
@@ -10,18 +10,38 @@ function writePassword() {
 
 }
 
-var specChar "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
+var specChar "!#$%&'()*+,-./:; <=>?@[\]^_`{|}~".split("");
 var lcLetters "abcdefghijklmnopqrstuvwxyz".split("");
 var ucLetters "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var numbers "0123456789".split("");
 
 
 
-var passLength = prompt("Enter a character length between 8 and 128 for the password.");
+var passLength = prompt("Enter the number of characters for the password (8-128).");
 var passLower = confirm("Should the password have lower case letters?");
 var passUpper = confirm("Should the password have upper case letters?");
 var passNum = confirm("Should the password contain numbers?");
 var passSpecChar = confirm("Should the password contain special characters?");
+
+for (i = 0; i < passLength; i++){
+  
+}
+
+if (passLower === true) {
+
+}
+
+if (passUpper === true) {
+
+}
+
+if (passNum === true) {
+
+}
+
+if (passSpecChar === true) {
+
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
